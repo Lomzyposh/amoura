98792b7dd4af37b7663c4645e9da4cf0d0a41a18
Lomzyposh <alaminolomo@gmail.com>
2025-12-15 18:36:42 +0100

    Remove server folder

--- server/routes/matchRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middleware/authMiddleware");
-const { getMatches } = require("../controllers/matchController");
-
-router.get("/", auth, getMatches);
-
-module.exports = router;
